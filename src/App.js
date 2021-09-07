2e006984c4f260188b7048ad4d4f7740599da24f
Jhon Steven Saavedra <jhon.giron@woombatcg.com>
2021-09-07 18:32:19 -0500

    Cambios en rutas, agrego el context (vacio)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { map } from 'lodash'
+import routes from './routes/routes.config'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div >
-      Buenas Buenas
-    </div>
+    <Router>
+      <Switch>
+        {
+          map(routes, (route, index) => (
+            <Route 
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          ))
+        }
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import WebFont from 'webfontloader';
 import App from './App';
+
+WebFont.load({
+  google: {
+    families: ['Roboto:400,500,700']
+  }
+});
 
 
 ReactDOM.render(
