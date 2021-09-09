2c4288b194f900e252042bfe79bbaa507d1d6137
Jhon Steven Saavedra <jhon.giron@woombatcg.com>
2021-09-08 21:39:02 -0500

    responsive en aliados agregado

--- src/style/Container.js
+++ src/style/Container.js
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: ${props => props.fluid ? '100%' : '78%'};
     margin: ${props => props.fluid ? '' : 'auto'};
+
+    @media (max-width: 60rem) {
+        width: 95%;
+    }
 `
