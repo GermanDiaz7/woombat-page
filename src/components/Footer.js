a278b92fe16cbb2564fb2af337b1c101d2df64b5
German Diaz <germanrodrigo.2003@hotmail.com>
2021-09-17 12:16:48 -0500

    listo para presentacion

---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import { Container } from '../style/Container';
+import { useTranslation } from "react-i18next";
+//styles
+import '../style/footer.css'
+
+const Footer = () => {
+
+    const { t } = useTranslation("global");
+
+    return ( 
+        <div className='footer-content'>
+            <Container>
+                <div className='content-grid'>
+                    <div className='footer div1'>
+                        <h5>{t("contacto.datos.email")}</h5>
+                        <h5>{t("contacto.datos.telefono")}</h5>
+                        <h5>{t("contacto.datos.Direccion")}</h5>
+                        <h5>{t("contacto.datos.ciudad")}</h5>
+                    </div>
+
+                    <div className='footer div2'>
+                        <a href="https://instaclone-steven.s3.us-west-1.amazonaws.com/politica-de-privacidad.pdf" target="_blank" rel="noopener noreferrer">Politica de privacidad</a>
+                        <h4>{t("navegacion.servicios")}</h4>
+                        <a href='#serivices'>{t("servicios.consultoria.titulo")}</a>
+                        <a href='#serivices'>{t("servicios.analitica-descriptiva.titulo")}</a>
+                        <a href='#serivices'>{t("servicios.analitica-avanzada.titulo")}</a>
+
+                    </div>
+
+                    <div className='footer div3'>
+                        <a href='#serivices'>{t("servicios.ingeligencia-artificial.titulo")}</a>
+                        <a href='#serivices'>{t("servicios.consultoria-nube.titulo")}</a>
+                        <h4>{t("navegacion.nosotros")}</h4>
+                        <a href='#about'>Mision</a>
+                        <a href='#about'>Vision</a>
+                    </div>
+
+                    <div className='footer div4'>
+                        <a href='#clients'>{t("navegacion.clientes")}</a>
+                        <a href='#contact'>{t("navegacion.contacto")}</a>
+                    </div>
+                </div>
+            </Container>
+        </div>
+     );
+}
+ 
+export default Footer;
